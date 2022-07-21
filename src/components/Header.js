c04ea7e992256eef940f5f91a1fdd036994244eb
Rifat Ahammed <reardj007@gmail.com>
2022-07-21 18:09:10 +0600

    Init

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Text , View, StyleSheet} from 'react-native';
+
+export default function Header() {
+    return (
+        <View>
+        <Text>Grab your</Text>
+        <Text>Delicous Meal!</Text> 
+        </View>
+        
+    );
+}
